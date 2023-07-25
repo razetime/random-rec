17637348698d7c0be7aea4a24d529940e5a5a0d5
razetime <raghuallthetime@hotmail.com>
2023-07-25 13:44:32 +0530

    d

--- code-guessing/cg25/Tests.hs
+++
@@ -1,4 +0,0 @@
-mat = [[False, True, True, False],
-       [False, False, True, True],
-       [True, True, False, True],
-       [False, False, True, False]]
--- code-guessing/cprun.sh
+++
@@ -1,2 +0,0 @@
-cp rm.pl rm1.pl
-perl rm1.pl $@
